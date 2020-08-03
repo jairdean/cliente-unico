7bfbc65b85d50e4b307fde258ea909f83add3981
jflores <jflores@vimeworks.com>
2020-08-03 11:14:24 -0500

    prueba del servicio

--- equivida-ws-cliente/src-databook/com/equivida/databook/client/impl/DatabookServiceImpl.java
+++ equivida-ws-cliente/src-databook/com/equivida/databook/client/impl/DatabookServiceImpl.java
@@ -110,6 +110,7 @@ public class DatabookServiceImpl implements DatabookService {
 			System.out.println(registrosEntity.getTitular().getPersona().getIdentificacion());
 			System.out.println(registrosEntity.getTitular().getDireccion().getSecParroquia());
 			System.out.println(registrosEntity.getTitular().getTelefonos().getTelefono1().getNroTelefono());
+			System.out.println(registrosEntity.getTitular().getInformacionAdicional().getCodActividadEconomica());
 			System.out.println("XX4");
 			//Registros registros = (Registros) jc.createUnmarshaller().unmarshal(xml); 
 			connection.disconnect();
